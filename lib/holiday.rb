03395dd136a45808d1b011ebc787597c48cdabf0
Kenny Hollaway <hollawaykenny007@gmail.com>
2020-09-19 18:34:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,9 +66,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    puts "#{season.capitalize}"
+    puts "#{season.capitalize}:"
     holiday.each do |day, item|
-     puts "#{day.to_s.split("_").map {|i| i.capitalize}.join(" ")}}: #{item.join(", ")}"
+     puts "  #{day.to_s.split("_").map {|i| i.capitalize}.join(" ")}}: #{item.join(", ")}"
    end
  end
       
