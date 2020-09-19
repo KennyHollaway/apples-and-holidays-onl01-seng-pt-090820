6d5ecffc130929717f374167c9516094751c9e76
Kenny Hollaway <hollawaykenny007@gmail.com>
2020-09-19 18:30:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,12 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}"
     holiday.each do |day, item|
      puts day.to_s.split("_").map {|i| i.capitalize}.join(" ")}:
-      end
-   end
- end
       
-      
-
 end
 
 def all_holidays_with_bbq(holiday_hash)
